cbab7c2601434a9eb7c36ddc8ebcbb1da3af5c40
[Josh Ackland] <[joshackland98@gmail.com]>
2020-12-02 13:22:35 +0800

    Completed Day 2

---
+++ day2/day2.py
@@ -0,0 +1,42 @@
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+file = open(dir_path + '/' + 'file.txt', 'r')
+Lines = file.readlines()
+
+#PART 1
+valid_passwords = 0
+
+for line in Lines:
+    l = line.split(' ')
+    l[0] = l[0].split('-')
+    l[1] = l[1].replace(':','')
+    l[2] = l[2].replace('\\n','')
+    
+    counter = 0
+
+    for letter in l[2]:
+        if l[1] == letter:
+            counter += 1
+
+    if counter >= int(l[0][0]) and counter <= int(l[0][1]):
+        valid_passwords += 1
+
+print(valid_passwords)
+
+
+#PART 2
+valid_passwords = 0
+
+for line in Lines:
+    l = line.split(' ')
+    l[0] = l[0].split('-')
+    l[1] = l[1].replace(':','')
+    l[2] = l[2].replace('\\n','')
+
+    if l[2][int(l[0][0])-1] == l[1] or l[2][int(l[0][1])-1] == l[1]:
+        valid_passwords += 1
+        if l[2][int(l[0][0])-1] == l[1] and l[2][int(l[0][1])-1] == l[1]:
+            valid_passwords -= 1
+
+print(valid_passwords)
