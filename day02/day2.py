39dc42de64a050845710360dcd3e79e128ff0acc
[Josh Ackland] <[joshackland98@gmail.com]>
2020-12-02 22:27:56 +0800

    updated day2 python

--- day02/day2.py
+++ day02/day2.py
@@ -10,6 +10,8 @@ valid_passwords = 0
 for line in Lines:
     l = line.split(' ')
     l[0] = l[0].split('-')
+    num1 = int(l[0][0])
+    num2 = int(l[0][1])
     l[1] = l[1].replace(':','')
     l[2] = l[2].replace('\\n','')
     
@@ -19,7 +21,7 @@ for line in Lines:
         if l[1] == letter:
             counter += 1
 
-    if counter >= int(l[0][0]) and counter <= int(l[0][1]):
+    if counter >= num1 and counter <= num2:
         valid_passwords += 1
 
 print(valid_passwords)
@@ -31,12 +33,14 @@ valid_passwords = 0
 for line in Lines:
     l = line.split(' ')
     l[0] = l[0].split('-')
+    num1 = int(l[0][0])
+    num2 = int(l[0][1])
     l[1] = l[1].replace(':','')
     l[2] = l[2].replace('\\n','')
 
-    if l[2][int(l[0][0])-1] == l[1] or l[2][int(l[0][1])-1] == l[1]:
+    if l[2][num1-1] == l[1] or l[2][num2-1] == l[1]:
         valid_passwords += 1
-        if l[2][int(l[0][0])-1] == l[1] and l[2][int(l[0][1])-1] == l[1]:
+        if l[2][num1-1] == l[1] and l[2][num2-1] == l[1]:
             valid_passwords -= 1
 
 print(valid_passwords)
